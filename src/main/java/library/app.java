271f9b1758621b55b123258b20d7720f5502cb78
usertam <heiyiutam@gmail.com>
2020-11-12 00:29:46 +0800

    library sqlite database foundations

--- src/main/java/library/app.java
+++ src/main/java/library/app.java
@@ -4,6 +4,9 @@ public class app {
 
     public static void main(String[] args) {
 
+        // connect to database
+        sqlite.connect();
+
         // process arguments, or start interactive cycles
         if (args.length != 0) {
             interactive.eval(args);
@@ -17,6 +20,7 @@ public class app {
 
         // stop running resources
         sc.kill();
+        sqlite.kill();
 
         // terminate the program
         System.exit(code);
