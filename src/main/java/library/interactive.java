3f399dd04e3dbc3892e3b3b502e4aab8fa7e545b
usertam <heiyiutam@gmail.com>
2020-11-21 21:41:43 +0800

    interactive.java: rewrite split method

--- src/main/java/library/interactive.java
+++ src/main/java/library/interactive.java
@@ -1,8 +1,12 @@
 package library;
 
 import java.util.Scanner;
+import java.util.List;
 import java.util.ArrayList;
 import java.util.Arrays;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class interactive {
 
@@ -11,9 +15,19 @@ public class interactive {
         // interactive cycles
         while (true) {
             String line = sc.prompt("> ");
-            String[] cmd = parser.spilt(line);
+            String[] cmd = split(line);
             eval(cmd);
         }
+    }
+
+    public static String[] split(String line) {
+
+        // use regex to split the line
+        List<String> cmd = new ArrayList<>();
+        Pattern p = Pattern.compile("[^\\s\"']+|\"[^\"]*\"|'[^']*'");
+        Matcher m = p.matcher(line);
+        while (m.find()) cmd.add(m.group().replaceAll("[\"']", ""));
+        return cmd.toArray(new String[0]);
     }
 
     public static void eval(String[] cmd) {
@@ -54,71 +68,6 @@ public class interactive {
                 break;
             default:
                 System.out.printf("[*] Unknown command: %s\n", Arrays.toString(cmd));
-        }
-    }
-
-    private static class parser {
-
-        private static String[] spilt(String line) {
-    
-            // create objects from classes
-            parser p = new parser();
-            parser.buf buf = p.new buf();
-            parser.del del = p.new del();
-    
-            // command parsing logic
-            char a[] = line.toCharArray();
-            for (char c : a) {
-                if (c == del.current) {
-                    buf.reset();
-                    del.reset();
-                }
-                else if (c == del.quote) {
-                    buf.reset();
-                    del.quote();
-                }
-                else {
-                    buf.append(c);
-                }
-            }
-    
-            // return a string array
-            return buf.get();
-        }
-    
-        private class buf {
-    
-            // define array list and string buffer
-            ArrayList<String> cmd = new ArrayList<>();
-            StringBuffer buf = new StringBuffer();
-    
-            // method to append char to buffer
-            void append(char c) {
-                buf.append(c);
-            }
-    
-            // method to append the string to arraylist, then clear the buffer
-            void reset() {
-                if (buf.length() > 0) cmd.add(buf.toString());
-                buf.delete(0, buf.length());
-            }
-    
-            // method to return a string array
-            String[] get() {
-                reset();
-                return cmd.stream().toArray(String[]::new);
-            }
-        }
-    
-        private class del {
-    
-            // define delimiter characters
-            char space = ' ', quote = '"';
-            char current = space;
-    
-            // methods to change current delimiter
-            void quote() { current = quote; }
-            void reset() { current = space; }
         }
     }
 }
