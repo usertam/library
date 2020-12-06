50cfb400da0c6f6a03d174a0852e90df23e3de70
usertam <heiyiutam@gmail.com>
2020-12-07 01:28:29 +0800

    interactive.java: fix typos in method help

--- src/main/java/library/interactive.java
+++ src/main/java/library/interactive.java
@@ -122,18 +122,18 @@ public class interactive {
     public static void help() {
         
         System.out.println("[*] Listing available commands.");
-        System.out.println("=".repeat(80));
+        System.out.println("=".repeat(76));
 
         // commands available for guests
         System.out.println("search\t<keywords>\t Search for a book using keywords.");
         System.out.println("\t\t\t   Keywords with spaces can be placed in a quote.");
         System.out.println("\t\t\t   e.g. search \"key words\" ");
         System.out.println("listbooks\t\t Display all library collections.");
-        System.out.println("=".repeat(80));
+        System.out.println("=".repeat(76));
         System.out.println("whoami\t\t\t Display the current user.");
-        System.out.println("login\t<user>\t\t Login as an user, providing password.");
+        System.out.println("login\t<user>\t\t Login as a user, providing password.");
         System.out.println("exit\t\t\t Terminate the program.");
-        System.out.println("=".repeat(80));
+        System.out.println("=".repeat(76));
 
         // commands available for users
         if (auth.check(auth.uid(), 1)) {
@@ -142,7 +142,7 @@ public class interactive {
             System.out.println("records\t\t\t Show your personal records.");
             System.out.println("passwd\t\t\t Change your user password.");
             System.out.println("logout\t\t\t Terminate your user session.");
-            System.out.println("=".repeat(80));
+            System.out.println("=".repeat(76));
         }
 
         // commands available for admin
@@ -150,14 +150,14 @@ public class interactive {
             System.out.println("su\t<user>\t\t Impersonate a specific user, without password.");
             System.out.println("\t\t\t   To return to the original user, use \"logout\". ");
             System.out.println("records\t<user>\t\t Show records of a specific user.");
-            System.out.println("listusers\t\t Display all user entires.");
             System.out.println("passwd\t<user>\t\t Change password of a specific user.");
-            System.out.println("=".repeat(80));
-            System.out.println("adduser\t<user>\t\t Add an user entry.");
-            System.out.println("deluser\t<user>\t\t Delete an user entry.");
+            System.out.println("listusers\t\t Display all user entries.");
+            System.out.println("=".repeat(76));
+            System.out.println("adduser\t<user>\t\t Add a user entry.");
+            System.out.println("deluser\t<user>\t\t Delete a user entry.");
             System.out.println("addbook\t<ISBN>\t\t Add a book entry.");
             System.out.println("delbook\t<ISBN>\t\t Delete a book entry.");
-            System.out.println("=".repeat(80));
+            System.out.println("=".repeat(76));
         }
     }
 }
